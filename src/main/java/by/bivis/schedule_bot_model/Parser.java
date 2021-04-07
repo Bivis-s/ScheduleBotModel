3cc9a5abac94025f48bd4338f9e5e86d86436aec
Bivis-s <typoborov@yandex.by>
2021-04-07 23:35:35 +0300

    Parser interface was updated

--- src/main/java/by/bivis/schedule_bot_model/Parser.java
+++ src/main/java/by/bivis/schedule_bot_model/Parser.java
@@ -1,17 +1,22 @@
 package by.bivis.schedule_bot_model;
 
-import by.bivis.schedule_bot_model.objects.parser_objects.News;
-import by.bivis.schedule_bot_model.objects.parser_objects.Schedule;
-import by.bivis.schedule_bot_model.objects.parser_objects.Source;
-
 import java.util.List;
 import java.util.Set;
 
-public interface Parser { // TODO Сделать прокси между парсером и моделью, связать с бд
-    List<News> getNews();
-    Schedule getSchedule(Source<?> source);
-    List<Source<?>> getSourceList();
-    List<Source<?>> getSourceList(String subcategory);
+public interface Parser<NEWS, SOURCE, SCHEDULE> { // TODO Сделать прокси между парсером и моделью, связать с бд
+    List<NEWS> getNews();
+
+    SCHEDULE getSchedule(SOURCE source);
+
+    List<SOURCE> getSources();
+
+    //TODO ----------------------------------------------
+    // Всё что ниже в отдельный класс, связанный с моделью
+    // т.к. брать данные надо не с сайта, соотв. связи с парсером нет
+
     Set<String> getSourceCategorySet();
+
     Set<String> getSourceSubcategoryList(String category);
+
+    Set<SOURCE> getSources(String category, String subcategory);
 }
