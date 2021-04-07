d4c70a3f2a901ea34f4e038931a667af78d5837d
Bivis-s <typoborov@yandex.by>
2021-04-07 23:37:15 +0300

    ScheduleBotModel was updated

--- src/main/java/by/bivis/schedule_bot_model/ScheduleBotModel.java
+++ src/main/java/by/bivis/schedule_bot_model/ScheduleBotModel.java
@@ -1,89 +1,103 @@
 package by.bivis.schedule_bot_model;
 
-import by.bivis.schedule_bot_model.objects.parser_objects.Schedule;
-import by.bivis.schedule_bot_model.objects.parser_objects.Source;
-import by.bivis.schedule_bot_model.objects.parser_objects.User;
-import by.bivis.schedule_bot_model.objects.parser_objects.UserState;
+import by.bivis.schedule_bot_model.objects.db_objects.*;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@RequiredArgsConstructor
+import java.util.List;
+
+@NoArgsConstructor
 @Getter
 @Setter
-public class ScheduleBotModel<T extends User> {
-    private final Dao dao;
-    private final Parser parser;
-    private final ScheduleBotView<T> view;
-    private UserState userState; //TODO Временное состояние юзера, заменить связью с бд
+public abstract class ScheduleBotModel<USER, NEWS, SOURCE, SCHEDULE> {
+    private NewsDao<NEWS> newsDao;
+    private SourceDao<SOURCE> sourceDao;
+    private ScheduleDao<SCHEDULE> scheduleDao;
+    private UserDao<USER> userDao;
+    private Parser<NEWS, SOURCE, SCHEDULE> parser;
+    private ScheduleBotView<USER, NEWS, SCHEDULE, SOURCE> view;
+
+    protected abstract USER setStateToUser(USER user, UserState state);
+
+    protected abstract USER setSelectedSourceCategoryToUser(USER user, String sourceCategory);
+
+    protected abstract USER setSelectedSourceSubcategoryToUser(USER user, String sourceSubcategory);
+
+    protected void updateUserState(USER user, UserState state) {
+        getUserDao().update(setStateToUser(user, state));
+    }
+
+    protected void updateUserSelectedSourceCategory(USER user, String category) {
+        getUserDao().update(setSelectedSourceCategoryToUser(user, category));
+    }
+
+    protected void updateUserSelectedSourceSubcategory(USER user, String subcategory) {
+        getUserDao().update(setSelectedSourceSubcategoryToUser(user, subcategory));
+    }
+
+    // TODO вынести в отдельный класс
+    private void updateSources(List<SOURCE> sources) {
+        getSourceDao().update(sources.get(2));
+    }
 
     public void updateSources() {
-        getDao().updateSources(getParser().getSourceList());
+        List<SOURCE> sources = getParser().getSources();
+        for (SOURCE source : sources) {
+            getSourceDao().update(source);
+        }
     }
 
     public void updateNews() {
-        getDao().updateNews(getParser().getNews());
+        List<NEWS> newsList = getParser().getNews();
+        for (NEWS news : newsList) {
+            getNewsDao().update(news);
+        }
     }
 
-    private void updateSchedule(Schedule schedule) {
-        getDao().updateSchedules(schedule);
+    private void updateSchedule(SCHEDULE schedule) {
+        getScheduleDao().update(schedule);
     }
 
-    public void updateSignedSourceSchedules() {
-        for (Source<?> source : getDao().getSignedSources()) {
+    public void updateSchedules() {
+        for (SOURCE source : getSourceDao().getSignedSources()) {
             updateSchedule(getParser().getSchedule(source));
         }
     }
 
-    public void sendNewsToView(T user) {
+    public void sendNewsToView(USER user) {
         getView().sendNews(user, getParser().getNews());
-        userState = UserState.NEWS; //TODO Заменить связью с бд
+        updateUserState(user, UserState.NEWS);
     }
 
-    public void sendSourceCategoriesToView(T user) {
+    public void sendSourceCategoriesToView(USER user) {
         getView().sendSourceCategories(user, getParser().getSourceCategorySet());
-        userState = UserState.SOURCE_CATEGORIES; //TODO Заменить связью с бд
+        updateUserState(user, UserState.PICK_SOURCE_CATEGORY);
     }
 
-    public void sendSourcesSubcategoryByCategoryToView(T user, String category) {
+    public void sendSourcesSubcategoryByCategoryToView(USER user, String category) {
         getView().sendSourcesSubcategoryByCategory(user, getParser().getSourceSubcategoryList(category));
-        userState = UserState.SOURCES; //TODO Заменить связью с бд
+        updateUserState(user, UserState.SOURCES_SUBCATEGORIES);
     }
 
-    public void sendSchedulesToView(T user) {
-        getView().sendSchedules(user, getDao().getSchedules(getDao().getUserSubscriptions(user)));
+    public void sendSelectedSource(USER user, String category, String subcategory) {
+        getView().sendSources(getSourceDao().getSourcesByCategoryAndSubcategory(category, subcategory));
     }
 
-    public void sendHelloMessageToView(T user) {
+    public void sendSubscriptionsToView(USER user, long userId) {
+        getView().sendSignedSources(user, getSourceDao().getSignedSources(userId));
+        updateUserState(user, UserState.PICK_SIGNED_SOURCE);
+    }
+
+    public void sendScheduleToView(USER user, SOURCE source) {
+        getView().sendSchedule(user, getScheduleDao().get(source));
+        updateUserState(user, UserState.SCHEDULE);
+    }
+
+    public void sendHelloMessageToView(USER user) {
         getView().sendHelloMessage(user, "Щеее не вмерла Украина!"); //TODO вынести весь статичный текст в отдельный класс/интерфейс
-        userState = UserState.START; //TODO Заменить связью с бд
+        updateUserState(user, UserState.START);
     }
 
-    public void handle(T user, String message) {
-        switch (message) {
-            case "/start": //TODO Вынести в отдельный интерфейс
-                sendHelloMessageToView(user);
-                break;
-            case "/news":
-                sendNewsToView(user);
-                break;
-            case "/subscribe":
-                sendSourceCategoriesToView(user);
-                break;
-            default:
-                switch (userState) {
-                    case SOURCE_CATEGORIES:
-                        sendSourcesSubcategoryByCategoryToView(user, message);
-                        break;
-                    case SOURCES_SUBCATEGORIES:
-                        //TODO ??
-                        break;
-                    case SOURCES:
-                        //TODO Implement
-                        break;
-                    default:
-                        break;
-                }
-        }
-    }
+    public abstract void handleCommandByUserState(USER user, String command);
 }
